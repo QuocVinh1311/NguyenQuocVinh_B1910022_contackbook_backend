d0a1b5dcb731675c385894aa92138ecd6034cf94
Vinh Nguyễn <vinhb1910022@student.ctu.edu.vn>
2023-02-15 10:19:41 +0700

    Dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -3,11 +3,29 @@ const cors = require("cors");
 
 const app = express();
 
+const contactsRouter = require("./app/routes/contact.route");
+
+const ApiError = require("./app/api-error");
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.post("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
+});
+
+app.use("/api/contacts", contactsRouter);
+
+//handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+
+});
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
 });
 
 module.exports = app;
