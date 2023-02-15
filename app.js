3ff432d22521daccba909ad6cea151ef6bd81b97
Vinh Nguyễn <vinhb1910022@student.ctu.edu.vn>
2023-02-15 10:34:41 +0700

    Cai dat xu li loi

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const ApiError = require("./app/api-error");
 app.use(cors());
 app.use(express.json());
 
-app.post("/", (req, res) => {
+app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 });
 
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 
 });
 
-app.use((err, req, res, next) => {
+app.use((error, req, res, next) => {
     return res.status(error.statusCode || 500).json({
         message: error.message || "Internal Server Error",
     });
