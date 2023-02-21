0dfb094882f0370561d4c25b4532371f075cf24b
Vinh Nguyễn <vinhb1910022@student.ctu.edu.vn>
2023-02-21 15:59:35 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
-const app = require('./app');
-const config = require('./app/config');
+// const app = require('./app');
+// const config = require('./app/config');
 
-//Start server
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}.`);
-});
+// //Start server
+// const PORT = config.app.port;
+// app.listen(PORT, () => {
+//     console.log(`server is running on port ${PORT}.`);
+// });
+
+//Kết nối CSDL MongoDB
+
+const app = require("./app");
+const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
+
+async function startServer() { //Async được dùng để khai báo một hàm bất đồng bộ
+    try {
+        await MongoDB.connect(config.db.uri); //Await làm cho JavaScript đợi cho đến khi promise trả về kết quả
+        console.log("Connected to the databases!");
+
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server is running on ${PORT}.`);
+        });
+    } catch (error) {
+        console.log("Can not connect to databases!", error);
+        process.exit();
+    }
+}
+
+startServer();
